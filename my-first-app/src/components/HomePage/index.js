5b1e8ec49c6f53a846c330395629ff5b1e5cf8e6
srigowrijatla <srigowrijatla@gmail.com>
2025-02-11 15:48:23 +0530

    Homepage with a login button

---
+++ my-first-app/src/components/HomePage/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+const HomePage = () => {
+  return (
+    <div>
+      <h1> This is the landing page</h1><button>login</button>{" "}
+    </div>
+  );
+};
+export default HomePage;
--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import ProductList from './ProductList/index'
-import reportWebVitals from './reportWebVitals';
-import ProductListApiVisibleTillLimitedTime from './ProductListApiVisibleTillLimitedTime/index'
+// import App from './App';
+// import ProductList from './ProductList/index'
+// import reportWebVitals from './reportWebVitals';
+// import ProductListApiVisibleTillLimitedTime from './ProductListApiVisibleTillLimitedTime/index'
+import HomePage from './components/HomePage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <ProductList/>
-    <ProductListApiVisibleTillLimitedTime/>
+    {/* <ProductList/>
+    <ProductListApiVisibleTillLimitedTime/> */}
+    <HomePage/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
