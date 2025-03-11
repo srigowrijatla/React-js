d5e4d9e7d60b5c699e93b487134d29d1527c5a2c
srigowrijatla <srigowrijatla@gmail.com>
2025-03-11 15:22:51 +0530

    added routing

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from './App';
+ import App from './App';
 import ProductList from "./ProductList/index";
 // import reportWebVitals from './reportWebVitals';
 import ProductListApiVisibleTillLimitedTime from "./ProductListApiVisibleTillLimitedTime/index";
@@ -10,10 +10,10 @@ import HomePage from "./components/HomePage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <ProductList />
+    <App />
+    {/* <ProductList />
     <ProductListApiVisibleTillLimitedTime />
-    <HomePage />
+    <HomePage /> */}
   </React.StrictMode>
 );
 
